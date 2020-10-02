90ae0a52d050d28021e2ff6ad9ae0036fc339dd9
Scott Pribe <Scottpribe@gmail.com>
2020-10-01 21:32:56 -0400

    adjusted the form and update the handleChange events as well as add feature of creating multiple components

--- src/components/experience.js
+++ src/components/experience.js
@@ -5,22 +5,30 @@ export default class Experience extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			jobFormsArray: [],
+			jobFormsArray: [<JobForm />],
 		};
 		this.handleAddJob = this.handleAddJob.bind(this);
 	}
 
-	handleAddJob() {}
+	handleAddJob() {
+		this.setState({
+			jobFormsArray: [...this.state.jobFormsArray, <JobForm />],
+		});
+	}
 
 	render() {
-		console.log(this.state.jobFormsArray);
 		return (
 			<main>
 				<h1>
 					<u>Experience</u>
 				</h1>
 				<button>Edit</button>
-				<JobForm />
+				<form id="experienceForm">
+					{this.state.jobFormsArray.map((job, i) => (
+						<JobForm key={job + i} />
+					))}
+					<input type="submit" value="Submit"></input>
+				</form>
 
 				<button onClick={this.handleAddJob} id="addJob">
 					+
--- src/components/forms.js
+++ src/components/forms.js
@@ -1,51 +1,88 @@
 import React from "react";
 export default class JobForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			companyName: "",
+			location: "",
+			roleTitle: "",
+			startDate: "",
+			endDate: "",
+			tasks: "",
+		};
+		this.handleChange = this.handleChange.bind(this);
+	}
+	handleChange(e) {
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	}
+	deleteJob() {
+		console.log("delete this current job!");
+	}
 	render() {
 		return (
-			<form id="jobForm">
-				<div id="job">
-					<label>
-						Company Name:
-						<input type="text" value="" placeholder="ABC Company"></input>
-					</label>
-					<label>
-						Location:
-						<input type="text" value="" placeholder="New York City, NY"></input>
-					</label>
-					<label>
-						Role:
-						<input
-							type="text"
-							value=""
-							placeholder="Software Developer"
-						></input>
-					</label>
-					<label>
-						Duration:
-						<input
-							type="text"
-							value=""
-							placeholder="9/30/2020 - Present
-                        "
-						></input>
-					</label>
-					<label>
-						Tasks
-						<ul>
-							<li>
-								<input type="text" value="" placeholder="task1"></input>
-							</li>
-							<li>
-								<input type="text" value="" placeholder="task2"></input>
-							</li>
-							<li>
-								<input type="text" value="" placeholder="task3"></input>
-							</li>
-						</ul>
-					</label>
-				</div>
-				<input type="submit" value="Submit" />
-			</form>
+			<div id="job">
+				<label>
+					Company Name:
+					<input
+						name="companyName"
+						type="text"
+						value={this.state.companyName}
+						onChange={this.handleChange}
+						placeholder="ABC Company"
+					></input>
+				</label>
+				<label>
+					Location:
+					<input
+						name="location"
+						type="text"
+						value={this.state.location}
+						onChange={this.handleChange}
+						placeholder="New York City, NY"
+					></input>
+				</label>
+				<label>
+					Role Title:
+					<input
+						name="roleTitle"
+						type="text"
+						value={this.state.roleTitle}
+						onChange={this.handleChange}
+						placeholder="Software Developer"
+					></input>
+				</label>
+				<label>
+					Start Date:
+					<input
+						name="startDate"
+						type="Date"
+						value={this.state.startDate}
+						onChange={this.handleChange}
+					></input>
+					End Date:
+					<input
+						name="endDate"
+						type="Date"
+						value={this.state.endDate}
+						onChange={this.handleChange}
+					></input>
+				</label>
+				<label>
+					Tasks
+					<textarea
+						name="tasks"
+						type="text"
+						value={this.state.tasks}
+						onChange={this.handleChange}
+						placeholder="Enter Tasks Here"
+					></textarea>
+				</label>
+				<button id="deleteJob" onClick={this.deleteJob}>
+					x
+				</button>
+			</div>
 		);
 	}
 }
