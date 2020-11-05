f98d3d0828dc473b5c5afdf736187931cd663060
Scott Pribe <Scottpribe@gmail.com>
2020-11-04 22:36:25 -0500

    need to figure out how to refactor hooks and localStorage

--- src/components/genInfo.js
+++ src/components/genInfo.js
@@ -1,54 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import EditAndSaveButton from "./editAndSaveButton";
 import LinksInformation from "./linksComponent";
 
-export default class GeneralInformation extends React.Component {
-	//THIS IS A NEW BRANCH TEST 2
-	constructor(props) {
-		super(props);
-		this.state = {
-			name: "John Smith",
-			phone: "555-555-5555",
-			email: "johnsmith@gmail.com",
-			github: "https://github.com/johnsmith",
-			linkedIn: "https://www.linkedin.com/in/johnsmith/",
-			formView: false,
-		};
-		this.handleChange = this.handleChange.bind(this);
-		this.toggleView = this.toggleView.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-		this.wrapperFunction = this.wrapperFunction.bind(this);
-		this.print = this.print.bind(this);
-	}
-	handleChange(e) {
-		this.setState({
+export default function GeneralInformation() {
+	const [genInfo, setGenInfo] = useState({
+		name: "John Smith",
+		phone: "555-555-5555",
+		email: "johnsmith@gmail.com",
+		github: "https://github.com/johnsmith",
+		linkedIn: "https://www.linkedin.com/in/johnsmith/",
+		formView: false,
+	});
+	function handleChange(e) {
+		setGenInfo({
 			[e.target.name]: e.target.value,
 		});
 	}
-	toggleView() {
-		this.setState({
-			formView: !this.state.formView,
+	function toggleView() {
+		setGenInfo({
+			formView: !genInfo.formView,
 		});
 	}
-	print() {
+	function print() {
 		window.print();
 	}
-	handleSubmit(e) {
-		const { name, phone, email, github, linkedIn } = this.state;
-		localStorage.setItem("name", JSON.stringify(name));
-		localStorage.setItem("phone", JSON.stringify(phone));
-		localStorage.setItem("email", JSON.stringify(email));
-		localStorage.setItem("github", JSON.stringify(github));
-		localStorage.setItem("linkedIn", JSON.stringify(linkedIn));
+	function handleSubmit(e) {
+		// setGenInfo({
+		// 	name: genInfo.name,
+		// 	phone: genInfo.phone,
+		// 	email: genInfo.email,
+		// 	github: genInfo.github,
+		// 	linkedIn: genInfo.linkedIn,
+		// 	formView: genInfo.formView,
+		// });
+		console.log(genInfo.name);
+		// localStorage.setItem("name", JSON.stringify(genInfo.name));
+		// localStorage.setItem("phone", JSON.stringify(genInfo.phone));
+		// localStorage.setItem("email", JSON.stringify(genInfo.email));
+		// localStorage.setItem("github", JSON.stringify(genInfo.github));
+		// localStorage.setItem("linkedIn", JSON.stringify(genInfo.linkedIn));
 		e.preventDefault();
 	}
 
-	wrapperFunction(e) {
+	function wrapperFunction(e) {
 		//when the save button is clicked, the toggleView function and submit button fire off so we can have 2 functions occur on 1 click
-		this.toggleView();
-		this.handleSubmit(e);
+		toggleView();
+		handleSubmit(e);
 	}
-	componentDidMount() {
+	function componentDidMount() {
 		let name = JSON.parse(localStorage.getItem("name"));
 		let phone = JSON.parse(localStorage.getItem("phone"));
 		let email = JSON.parse(localStorage.getItem("email"));
@@ -61,111 +60,110 @@ export default class GeneralInformation extends React.Component {
 			github === null ||
 			linkedIn === null
 		) {
-			name = this.state.name;
-			phone = this.state.phone;
-			email = this.state.email;
-			github = this.state.github;
-			linkedIn = this.state.linkedIn;
+			name = genInfo.name;
+			phone = genInfo.phone;
+			email = genInfo.email;
+			github = genInfo.github;
+			linkedIn = genInfo.linkedIn;
 		}
-		this.setState({ name, phone, email, github, linkedIn });
+		setGenInfo({ name, phone, email, github, linkedIn });
 	}
-	render() {
-		if (this.state.formView === false) {
-			return (
-				<aside id="generalInfoContainer">
-					<div id="headerContainer">
-						<h1 id="nameHeader">{this.state.name}</h1>
-						<h3 id="phoneHeader">{this.state.phone}</h3>
-					</div>
-					<LinksInformation urls={this.state} print={this.print} />
-					<EditAndSaveButton
-						section="generalInfo"
-						view={this.state.formView}
-						toggleView={this.toggleView}
-						wrapperFunction={this.wrapperFunction}
-					/>
-				</aside>
-			);
-		} else {
-			return (
-				<aside id="generalInfoContainer">
-					<div id="headerContainer">
-						<form id="generalInfoForm" onSubmit={this.handleSubmit}>
-							<label>
-								<h4>Name:</h4>
-								<input
-									className="formInputs"
-									id="nameInput"
-									name="name"
-									type="text"
-									value={this.state.name}
-									onChange={this.handleChange}
-									placeholder="John Smith"
-								></input>
-							</label>
-							<br></br>
-							<label>
-								<h4>Phone:</h4>
-								<input
-									className="formInputs"
-									id="phoneInput"
-									name="phone"
-									type="text"
-									value={this.state.phone}
-									onChange={this.handleChange}
-									placeholder="555-555-5555"
-								></input>
-							</label>
-							<br></br>
-							<label>
-								<h4>Email:</h4>
-								<input
-									className="formInputs"
-									id="emailInput"
-									name="email"
-									type="text"
-									value={this.state.email}
-									onChange={this.handleChange}
-									placeholder="JohnSmith@gmail.com"
-								></input>
-							</label>
-							<br></br>
-							<label>
-								<h4>Github:</h4>
-								<input
-									className="formInputs"
-									id="gitHubInput"
-									name="github"
-									type="text"
-									value={this.state.github}
-									onChange={this.handleChange}
-									placeholder="https://..."
-								></input>
-							</label>
-							<br></br>
-							<label>
-								<h4>LinkedIn:</h4>
-								<input
-									className="formInputs"
-									id="linkedInInput"
-									name="linkedIn "
-									type="text"
-									value={this.state.linkedIn}
-									onChange={this.handleChange}
-									placeholder="https://..."
-								></input>
-							</label>
-							<br></br>
-							<EditAndSaveButton
-								section="generalInfo"
-								view={this.state.formView}
-								toggleView={this.toggleView}
-								wrapperFunction={this.wrapperFunction}
-							/>
-						</form>
-					</div>
-				</aside>
-			);
-		}
+
+	if (genInfo.formView === false) {
+		return (
+			<aside id="generalInfoContainer">
+				<div id="headerContainer">
+					<h1 id="nameHeader">{genInfo.name}</h1>
+					<h3 id="phoneHeader">{genInfo.phone}</h3>
+				</div>
+				<LinksInformation urls={genInfo} print={print} />
+				<EditAndSaveButton
+					section="generalInfo"
+					view={genInfo.formView}
+					toggleView={toggleView}
+					wrapperFunction={wrapperFunction}
+				/>
+			</aside>
+		);
+	} else {
+		return (
+			<aside id="generalInfoContainer">
+				<div id="headerContainer">
+					<form id="generalInfoForm" onSubmit={handleSubmit}>
+						<label>
+							<h4>Name:</h4>
+							<input
+								className="formInputs"
+								id="nameInput"
+								name="name"
+								type="text"
+								value={genInfo.name}
+								onChange={(e) => handleChange(e)}
+								placeholder="John Smith"
+							></input>
+						</label>
+						<br></br>
+						<label>
+							<h4>Phone:</h4>
+							<input
+								className="formInputs"
+								id="phoneInput"
+								name="phone"
+								type="text"
+								value={genInfo.phone}
+								onChange={(e) => handleChange(e)}
+								placeholder="555-555-5555"
+							></input>
+						</label>
+						<br></br>
+						<label>
+							<h4>Email:</h4>
+							<input
+								className="formInputs"
+								id="emailInput"
+								name="email"
+								type="text"
+								value={genInfo.email}
+								onChange={(e) => handleChange(e)}
+								placeholder="JohnSmith@gmail.com"
+							></input>
+						</label>
+						<br></br>
+						<label>
+							<h4>Github:</h4>
+							<input
+								className="formInputs"
+								id="gitHubInput"
+								name="github"
+								type="text"
+								value={genInfo.github}
+								onChange={(e) => handleChange(e)}
+								placeholder="https://..."
+							></input>
+						</label>
+						<br></br>
+						<label>
+							<h4>LinkedIn:</h4>
+							<input
+								className="formInputs"
+								id="linkedInInput"
+								name="linkedIn "
+								type="text"
+								value={genInfo.linkedIn}
+								onChange={(e) => handleChange(e)}
+								placeholder="https://..."
+							></input>
+						</label>
+						<br></br>
+						<EditAndSaveButton
+							section="generalInfo"
+							view={genInfo.formView}
+							toggleView={toggleView}
+							wrapperFunction={wrapperFunction}
+						/>
+					</form>
+				</div>
+			</aside>
+		);
 	}
 }
